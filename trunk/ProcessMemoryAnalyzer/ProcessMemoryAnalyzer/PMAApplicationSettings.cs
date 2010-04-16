5233caf494ca1a6203f1ecb00f2f97094ba2643e
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2010-04-16 12:21:00 +0000

    Initial Build

---
+++ trunk/ProcessMemoryAnalyzer/ProcessMemoryAnalyzer/PMAApplicationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PMA.ProcessMemoryAnalyzer
+{
+    public class PMAApplicationSettings
+    {
+        
+        public static string PMAApplicationDirectory
+        {
+            get { return Path.GetDirectoryName(Application.ExecutablePath); }
+        }
+
+        public static string PMAApplicationDirectoryConfig
+        {
+            get { return Path.GetDirectoryName(Application.ExecutablePath)+"\\Config"; }
+        }
+    }
+
+}
+
---
+++ trunk/ProcessMemoryAnalyzer/ProcessMemoryAnalyzer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PMA.Utils.smtp;
+using PMA.Utils;
+using System.IO;
+using GoogleChartSharp;
+using System.Diagnostics;
+
+
+namespace PMA.ProcessMemoryAnalyzer
+{
+    public class Program
+    {
+
+        const int SPACE_LENGTH = 20;
+
+        PMAInfo pmaInfo;
+        Emails emailsInfo;
+        SmtpInfo smtpInfo;
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.SerializeObject();
+            p.DeserilizeObjects();
+
+            new PMATaskHandler().RunTask();
+
+           // Environment.
+
+        }
+
+        
+        private void SerializeObject()
+        {
+            pmaInfo = new PMAInfo();
+
+            pmaInfo.LogingTimeInterval = 0;
+            pmaInfo.MailingTime = DateTime.Now.ToShortTimeString();
+
+            //pmaInfo.ReportsInterval = "DAILY";
+            pmaInfo.ServicesNames = new List<string>();
+            pmaInfo.ServicesNames.Add("MSSQLSERVER");
+
+            emailsInfo = new Emails();
+            emailsInfo.EmailTo = new List<string>();
+            emailsInfo.EmailTo.Add("msareen@ivp.in");
+            emailsInfo.EmailCC = new List<string>();
+            emailsInfo.EmailCC.Add("manasvi.sareen@gmail.com");
+            emailsInfo.AttachmentPath = "";
+            emailsInfo.Subject = "Server Report";
+            emailsInfo.BodyContent = "Please Find the Report Attached";
+
+            smtpInfo = new SmtpInfo();
+            smtpInfo.UserName = "productitsupport@cosmostech.in";
+            smtpInfo.Password = "123@cosmos";
+            smtpInfo.Port = 587;
+            smtpInfo.SmtpServer = "smtp.gmail.com";
+            smtpInfo.SSL = true;
+            smtpInfo.TimeOut = 100000;
+
+            SerializedInfo();
+        }
+
+        private void SerializedInfo()
+        {
+            File.WriteAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectoryConfig, PMAInfo.PMA_INFO_FILE), PMAInfoObj.Serialize());
+
+            File.WriteAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectoryConfig, Emails.EMAILS_INFO_FILE), EmailsInfoObj.Serialize());
+
+            File.WriteAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectoryConfig, SmtpInfo.SMTP_INFO_FILE), SmtpInfoObj.Serialize());
+        }
+
+        private void DeserilizeObjects()
+        {
+            pmaInfo = PMAInfo.Deserialize(File.ReadAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectory, PMAInfo.PMA_INFO_FILE)));
+
+            emailsInfo = Emails.Deserialize(File.ReadAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectory, Emails.EMAILS_INFO_FILE)));
+
+            smtpInfo = SmtpInfo.Deserialize(File.ReadAllText(Path.Combine(PMAApplicationSettings.PMAApplicationDirectory, SmtpInfo.SMTP_INFO_FILE)));
+
+        }
+
+
+        
+    }
+}
