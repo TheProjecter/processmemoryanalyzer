ec4f743d67980e6aba2052920097577a3ad1ba0f
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2010-04-20 15:26:39 +0000

    Currently Contains EncryptDecrypt Method

---
+++ ProcessMemoryAnalyzer/PMAUtils/OperationUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMA.Utils
+{
+    public class OperationUtils
+    {
+        //----------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Encrypts decrypt text provided with the key.
+        /// </summary>
+        /// <param name="textToEncrypt">The text to encrypt.</param>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public static string EncryptDecrypt(string textToEncrypt, int key)
+        {
+            StringBuilder inSb = new StringBuilder(textToEncrypt);
+            StringBuilder outSb = new StringBuilder(textToEncrypt.Length);
+            char c;
+            for (int i = 0; i < textToEncrypt.Length; i++)
+            {
+                c = inSb[i];
+                c = (char)(c ^ key);
+                outSb.Append(c);
+            }
+            return outSb.ToString();
+        }   
+
+    }
+}
