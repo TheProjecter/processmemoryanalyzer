071db151c5e8b0414afa65a5f72852fb1af2afea
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2010-04-20 06:07:21 +0000

    FTP Uploaded Checked in

--- trunk/ProcessMemoryAnalyzer/PMAUtils/FTP/FTPInfo.cs
+++ trunk/ProcessMemoryAnalyzer/PMAUtils/FTP/FTPInfo.cs
@@ -1,39 +1,51 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.IO;
-//using System.Xml.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
 
-//namespace PMA.Utils.ftp
-//{
+namespace PMA.Utils.ftp
+{
 
-//    public class FTPInfo
-//    {
-//        public const string FTP_INFO_FILE = "FTPInfo.xml";
+    public class FTPInfo
+    {
+        public const string FTP_INFO_FILE = "FTPInfo.xml";
 
-//        public string UserName { get; set; }
-//        public string Password { get; set; }
-//        public string FTPServer { get; set; }
-//        public string FTPServerFolder { get; set; }
-//        public int Port { get; set; }
-//        public int TimeOut { get; set; }
-//        public bool SSL { get; set; }
+        private string _password;
 
-//        public string Serialize()
-//        {
-//            StringWriter sw = new StringWriter();
-//            XmlSerializer x = new XmlSerializer(this.GetType());
-//            x.Serialize(sw, this);
-//            return sw.ToString();
-//        }
+        public string UserName { get; set; }
+        public string Password
+        {
+            get
+            {
+                return OperationUtils.EncryptDecrypt(_password, 129);
+            }
+            set
+            {
+                _password = OperationUtils.EncryptDecrypt(value, 129);
+            }
+        }
+        public string FTPServer { get; set; }
+        public string FTPServerFolder { get; set; }
+        public int Port { get; set; }
+        public int TimeOut { get; set; }
+        public bool SSL { get; set; }
 
-//        public static FTPInfo Deserialize(string strObject)
-//        {
-//            XmlSerializer x = new XmlSerializer(typeof(FTPInfo));
-//            return (FTPInfo)x.Deserialize(new StringReader(strObject));
-//        }
-//    }
+        public string Serialize()
+        {
+            StringWriter sw = new StringWriter();
+            XmlSerializer x = new XmlSerializer(this.GetType());
+            x.Serialize(sw, this);
+            return sw.ToString();
+        }
+
+        public static FTPInfo Deserialize(string strObject)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(FTPInfo));
+            return (FTPInfo)x.Deserialize(new StringReader(strObject));
+        }
+    }
 
 
-//}
+}
