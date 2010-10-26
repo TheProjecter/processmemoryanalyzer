10419407c8423bf1a722a8c6089dd9f1936200cc
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2010-10-26 14:51:37 +0000

    added installer in service

--- trunk/ProcessMemoryAnalyzer/PMAAlertService/ProjectInstaller.cs
+++ trunk/ProcessMemoryAnalyzer/PMAAlertService/ProjectInstaller.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 
 namespace PMA.PMAService
@@ -14,6 +17,44 @@ namespace PMA.PMAService
         public ProjectInstaller()
         {
             InitializeComponent();
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            
+            base.Install(stateSaver);
+            Action("i");
+
+        }
+
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            Action("u");
+        }
+
+
+        private void Action(string action)
+        {
+            Process ps = new Process();
+            ps.StartInfo.FileName = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe";
+            ps.StartInfo.Arguments = "PMAAlertService.exe -i";
+            ps.StartInfo.CreateNoWindow = false;
+            if (action == "u")
+            {
+                ps.StartInfo.Arguments = "PMAAlertService.exe -u";
+            }
+            else if (action == "i")
+            {
+                ps.StartInfo.Arguments = "PMAAlertService.exe -i";
+            }
+            ps.Start();
+            ps.WaitForExit();
         }
     }
 }
