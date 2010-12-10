b4233b5fdf01ca41b9d596f939c3ab653c246318
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2010-12-10 12:48:09 +0000

    Adding support to run in Vista and above OS

--- trunk/ProcessMemoryAnalyzer/PMASysAlertsUI/Program.cs
+++ trunk/ProcessMemoryAnalyzer/PMASysAlertsUI/Program.cs
@@ -12,7 +12,34 @@ namespace PMASysAlertsUI
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
+        {
+            if (Environment.OSVersion.Version.Major > 5 && args.Length == 0)
+            {
+                Process uiLuncher = new Process();
+                uiLuncher.StartInfo = new ProcessStartInfo(Environment.CurrentDirectory + "\\PMASysAlertsUI.exe", "userauth");
+                uiLuncher.StartInfo.Verb = "runas";
+                uiLuncher.Start();
+                System.Threading.Thread.Sleep(3000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                LaunchUI();
+            }
+
+            if (Environment.OSVersion.Version.Major > 5 && (args.Length == 1 && args[0] == "userauth"))
+            {
+                LaunchUI();
+            }
+            
+            
+        }
+
+        /// <summary>
+        /// Launches the UI.
+        /// </summary>
+        static void LaunchUI()
         {
             Process[] p = Process.GetProcessesByName("PMASysAlertsUI");
             if (p.Length > 1)
