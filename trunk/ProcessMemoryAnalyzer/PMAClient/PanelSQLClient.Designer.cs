108a2167d67f7424e4f9d14fbaca8b9b9db538e8
manasvi.sareen <manasvi.sareen@8604d5a1-e8b3-7a35-1fd4-419df1171fb1>
2011-02-25 17:09:54 +0000

    Added Pre script functionality

--- trunk/ProcessMemoryAnalyzer/PMAClient/PanelSQLClient.Designer.cs
+++ trunk/ProcessMemoryAnalyzer/PMAClient/PanelSQLClient.Designer.cs
@@ -39,14 +39,11 @@
             this.numericUpDown_Records = new System.Windows.Forms.NumericUpDown();
             this.label_NumberOfRecords = new System.Windows.Forms.Label();
             this.label_sqlServerName = new System.Windows.Forms.Label();
-            this.tabControl_SQL = new System.Windows.Forms.TabControl();
-            this.tabPage_SQL = new System.Windows.Forms.TabPage();
-            this.tabPage_Scripts = new System.Windows.Forms.TabPage();
             this.label_query = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_PreScripts = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_SQLResults)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Records)).BeginInit();
-            this.tabControl_SQL.SuspendLayout();
-            this.tabPage_SQL.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView_SQLResults
@@ -54,16 +51,16 @@
             this.dataGridView_SQLResults.AllowUserToAddRows = false;
             this.dataGridView_SQLResults.AllowUserToDeleteRows = false;
             this.dataGridView_SQLResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_SQLResults.Location = new System.Drawing.Point(18, 204);
+            this.dataGridView_SQLResults.Location = new System.Drawing.Point(18, 194);
             this.dataGridView_SQLResults.Name = "dataGridView_SQLResults";
-            this.dataGridView_SQLResults.Size = new System.Drawing.Size(815, 225);
+            this.dataGridView_SQLResults.Size = new System.Drawing.Size(815, 236);
             this.dataGridView_SQLResults.TabIndex = 0;
             // 
             // richTextBox_Query
             // 
-            this.richTextBox_Query.Location = new System.Drawing.Point(18, 123);
+            this.richTextBox_Query.Location = new System.Drawing.Point(18, 97);
             this.richTextBox_Query.Name = "richTextBox_Query";
-            this.richTextBox_Query.Size = new System.Drawing.Size(815, 75);
+            this.richTextBox_Query.Size = new System.Drawing.Size(815, 62);
             this.richTextBox_Query.TabIndex = 1;
             this.richTextBox_Query.Text = "";
             this.richTextBox_Query.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox_Query_KeyUp);
@@ -71,7 +68,7 @@
             // comboBox_Databases
             // 
             this.comboBox_Databases.FormattingEnabled = true;
-            this.comboBox_Databases.Location = new System.Drawing.Point(62, 8);
+            this.comboBox_Databases.Location = new System.Drawing.Point(78, 36);
             this.comboBox_Databases.Name = "comboBox_Databases";
             this.comboBox_Databases.Size = new System.Drawing.Size(119, 21);
             this.comboBox_Databases.TabIndex = 2;
@@ -79,7 +76,7 @@
             // label_Databases
             // 
             this.label_Databases.AutoSize = true;
-            this.label_Databases.Location = new System.Drawing.Point(3, 11);
+            this.label_Databases.Location = new System.Drawing.Point(19, 39);
             this.label_Databases.Name = "label_Databases";
             this.label_Databases.Size = new System.Drawing.Size(53, 13);
             this.label_Databases.TabIndex = 3;
@@ -97,9 +94,9 @@
             // 
             // button_Execute
             // 
-            this.button_Execute.Location = new System.Drawing.Point(758, 93);
+            this.button_Execute.Location = new System.Drawing.Point(758, 164);
             this.button_Execute.Name = "button_Execute";
-            this.button_Execute.Size = new System.Drawing.Size(75, 19);
+            this.button_Execute.Size = new System.Drawing.Size(75, 24);
             this.button_Execute.TabIndex = 6;
             this.button_Execute.Text = "Execute";
             this.button_Execute.UseVisualStyleBackColor = true;
@@ -111,7 +108,7 @@
             this.comboBox_queryType.Items.AddRange(new object[] {
             "SelectQuery",
             "NonQuery"});
-            this.comboBox_queryType.Location = new System.Drawing.Point(274, 8);
+            this.comboBox_queryType.Location = new System.Drawing.Point(279, 36);
             this.comboBox_queryType.Name = "comboBox_queryType";
             this.comboBox_queryType.Size = new System.Drawing.Size(121, 21);
             this.comboBox_queryType.TabIndex = 7;
@@ -120,7 +117,7 @@
             // label_queryType
             // 
             this.label_queryType.AutoSize = true;
-            this.label_queryType.Location = new System.Drawing.Point(209, 11);
+            this.label_queryType.Location = new System.Drawing.Point(214, 39);
             this.label_queryType.Name = "label_queryType";
             this.label_queryType.Size = new System.Drawing.Size(59, 13);
             this.label_queryType.TabIndex = 8;
@@ -128,7 +125,7 @@
             // 
             // numericUpDown_Records
             // 
-            this.numericUpDown_Records.Location = new System.Drawing.Point(493, 9);
+            this.numericUpDown_Records.Location = new System.Drawing.Point(496, 36);
             this.numericUpDown_Records.Maximum = new decimal(new int[] {
             5000,
             0,
@@ -141,7 +138,7 @@
             // label_NumberOfRecords
             // 
             this.label_NumberOfRecords.AutoSize = true;
-            this.label_NumberOfRecords.Location = new System.Drawing.Point(417, 11);
+            this.label_NumberOfRecords.Location = new System.Drawing.Point(420, 39);
             this.label_NumberOfRecords.Name = "label_NumberOfRecords";
             this.label_NumberOfRecords.Size = new System.Drawing.Size(70, 13);
             this.label_NumberOfRecords.TabIndex = 10;
@@ -156,58 +153,47 @@
             this.label_sqlServerName.TabIndex = 11;
             this.label_sqlServerName.Text = "<serverName>";
             // 
-            // tabControl_SQL
-            // 
-            this.tabControl_SQL.Controls.Add(this.tabPage_SQL);
-            this.tabControl_SQL.Controls.Add(this.tabPage_Scripts);
-            this.tabControl_SQL.Location = new System.Drawing.Point(18, 26);
-            this.tabControl_SQL.Name = "tabControl_SQL";
-            this.tabControl_SQL.SelectedIndex = 0;
-            this.tabControl_SQL.Size = new System.Drawing.Size(815, 63);
-            this.tabControl_SQL.TabIndex = 12;
-            // 
-            // tabPage_SQL
-            // 
-            this.tabPage_SQL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.tabPage_SQL.Controls.Add(this.label_Databases);
-            this.tabPage_SQL.Controls.Add(this.comboBox_Databases);
-            this.tabPage_SQL.Controls.Add(this.numericUpDown_Records);
-            this.tabPage_SQL.Controls.Add(this.label_NumberOfRecords);
-            this.tabPage_SQL.Controls.Add(this.label_queryType);
-            this.tabPage_SQL.Controls.Add(this.comboBox_queryType);
-            this.tabPage_SQL.Location = new System.Drawing.Point(4, 22);
-            this.tabPage_SQL.Name = "tabPage_SQL";
-            this.tabPage_SQL.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_SQL.Size = new System.Drawing.Size(807, 37);
-            this.tabPage_SQL.TabIndex = 1;
-            this.tabPage_SQL.Text = "SQL";
-            this.tabPage_SQL.Click += new System.EventHandler(this.tabPage_SQL_Click);
-            // 
-            // tabPage_Scripts
-            // 
-            this.tabPage_Scripts.Location = new System.Drawing.Point(4, 22);
-            this.tabPage_Scripts.Name = "tabPage_Scripts";
-            this.tabPage_Scripts.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_Scripts.Size = new System.Drawing.Size(807, 37);
-            this.tabPage_Scripts.TabIndex = 2;
-            this.tabPage_Scripts.Text = "Pre Scripts";
-            this.tabPage_Scripts.UseVisualStyleBackColor = true;
-            // 
             // label_query
             // 
             this.label_query.AutoSize = true;
-            this.label_query.Location = new System.Drawing.Point(19, 96);
+            this.label_query.Location = new System.Drawing.Point(19, 73);
             this.label_query.Name = "label_query";
             this.label_query.Size = new System.Drawing.Size(38, 13);
             this.label_query.TabIndex = 13;
             this.label_query.Text = "T-SQL";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Pre Scripts";
+            // 
+            // comboBox_PreScripts
+            // 
+            this.comboBox_PreScripts.FormattingEnabled = true;
+            this.comboBox_PreScripts.Location = new System.Drawing.Point(683, 36);
+            this.comboBox_PreScripts.Name = "comboBox_PreScripts";
+            this.comboBox_PreScripts.Size = new System.Drawing.Size(150, 21);
+            this.comboBox_PreScripts.TabIndex = 15;
+            this.comboBox_PreScripts.SelectedIndexChanged += new System.EventHandler(this.comboBox_PreScripts_SelectedIndexChanged);
+            // 
             // PanelSQLClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Controls.Add(this.comboBox_PreScripts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Databases);
+            this.Controls.Add(this.comboBox_Databases);
             this.Controls.Add(this.label_query);
-            this.Controls.Add(this.tabControl_SQL);
+            this.Controls.Add(this.label_queryType);
+            this.Controls.Add(this.label_NumberOfRecords);
+            this.Controls.Add(this.comboBox_queryType);
+            this.Controls.Add(this.numericUpDown_Records);
             this.Controls.Add(this.button_Execute);
             this.Controls.Add(this.label_sqlServerName);
             this.Controls.Add(this.label_SQLClientScreen);
@@ -217,9 +203,6 @@
             this.Size = new System.Drawing.Size(850, 450);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_SQLResults)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Records)).EndInit();
-            this.tabControl_SQL.ResumeLayout(false);
-            this.tabPage_SQL.ResumeLayout(false);
-            this.tabPage_SQL.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -238,9 +221,8 @@
         private System.Windows.Forms.NumericUpDown numericUpDown_Records;
         private System.Windows.Forms.Label label_NumberOfRecords;
         private System.Windows.Forms.Label label_sqlServerName;
-        private System.Windows.Forms.TabControl tabControl_SQL;
-        private System.Windows.Forms.TabPage tabPage_SQL;
-        private System.Windows.Forms.TabPage tabPage_Scripts;
         private System.Windows.Forms.Label label_query;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_PreScripts;
     }
 }
